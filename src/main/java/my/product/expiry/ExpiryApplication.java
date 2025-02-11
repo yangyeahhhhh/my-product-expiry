aab90829b5ae70d3819d99415d34cfeef27a1af4
yewon <ipunwon_@naver.com>
2025-02-11 20:39:12 +0900

    Set up project structure

---
+++ src/main/java/my/product/expiry/ExpiryApplication.java
@@ -0,0 +1,13 @@
+package my.product.expiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpiryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/product/expiry/ExpiryApplicationTests.java
@@ -0,0 +1,13 @@
+package my.product.expiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
